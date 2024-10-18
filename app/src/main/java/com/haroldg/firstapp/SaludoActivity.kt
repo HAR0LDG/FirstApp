b36d34b075af7effaa80acbe4780c40fe376d038
HAR0LDG <he.gonzalez100@gmail.com>
2024-10-17 19:16:29 -0500

    agregada la segunda vista Saludo y la funcionalidad al boton

--- app/src/main/java/com/haroldg/firstapp/SaludoActivity.kt
+++ app/src/main/java/com/haroldg/firstapp/SaludoActivity.kt
@@ -1,40 +1,26 @@
 package com.haroldg.firstapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class SaludoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_saludo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val saludo = findViewById<AppCompatTextView>(R.id.textsaludo)
+        val name = intent.extras?.getString("EXTRA-NAME").orEmpty()
 
-    val boton = findViewById<AppCompatButton>(R.id.boton)
-    val campo_Texto = findViewById<AppCompatEditText>(R.id.textfield)
-    var titulo = findViewById<AppCompatTextView>(R.id.titulo)
-
-    boton.setOnClickListener{
-        val name = campo_Texto.text.toString()
-        if(name.isNotEmpty()){
-            val intent = Intent(this, SaludoActivity::class.java)
-            intent.putExtra("EXTRA-NAME", name)
-            startActivity(intent)
-        }
+        saludo.text = "Hola $name 😊"
     }
 
-
-    }
 }
