a86ef29e1292afe9396d287559db41b20cb8ace4
HAR0LDG <he.gonzalez100@gmail.com>
2024-10-17 19:23:59 -0500

    agregado log a consola al pulsar boton

--- app/src/main/java/com/haroldg/firstapp/HomeActivity.kt
+++ app/src/main/java/com/haroldg/firstapp/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.haroldg.firstapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -30,6 +31,7 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, SaludoActivity::class.java)
             intent.putExtra("EXTRA-NAME", name)
             startActivity(intent)
+            Log.i("haroldg","Btn pulsado $name")
         }
     }
 
