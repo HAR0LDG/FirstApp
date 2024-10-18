a4ada56dbcb5d961d3806111c40a47898fab36d0
HAR0LDG <he.gonzalez100@gmail.com>
2024-10-17 20:10:12 -0500

    Cambio de pagina principal por menu, boton saludar funcional

--- app/src/main/java/com/haroldg/firstapp/Menu/MenuActivity.kt
+++ app/src/main/java/com/haroldg/firstapp/Menu/MenuActivity.kt
@@ -1,11 +1,15 @@
 package com.haroldg.firstapp.Menu
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.haroldg.firstapp.HomeActivity
 import com.haroldg.firstapp.R
+import com.haroldg.firstapp.SaludoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +21,15 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnSaludar = findViewById<Button>(R.id.btnSaludar)
+        btnSaludar.setOnClickListener{
+            navigateToSaludoApp()
+        }
+    }
+
+    private fun navigateToSaludoApp(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
